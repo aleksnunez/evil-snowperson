37d966596f2d7b537676de6efafd47021b29a67d
Alex Nunez <nunezaleks@gmail.com>
2021-11-03 12:33:05 -0700

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <h1 class="text-blue-400 text-center text-4xl mt-10">Evil Snowperson</h1>
+
+        </>
+    )
+}
+
+export default Header;

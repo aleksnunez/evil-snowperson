63e285d59e050bd9fb870467831505974a589078
Alex Nunez <nunezaleks@gmail.com>
2021-11-03 12:35:39 -0700

    rename file and add eyes

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -1,31 +1,39 @@
 import React from "react";
 import Sketch from "react-p5";
 
-	let x = 50;
-	let y = 50;
+
+	let buttonsx = 350;
+	let buttonsy = 340;
+
 const Figure = () => {
 	const setup = (p5, canvasParentRef) => {
-		p5.createCanvas(500, 500).parent(canvasParentRef);
+		let canvas = p5.createCanvas(700, 700).parent(canvasParentRef);
+
+		canvas.parent('sketch-holder');
 	};
 
 	const draw = (p5) => {
-		p5.background(220);
+		p5.background(120);
+		p5.noStroke()
+        
+		//snowman
+		p5.fill(255)
+		p5.ellipse(350, 500, 200, 200);
+		p5.ellipse(350, 375, 150, 150);
+		p5.ellipse(350, 265, 110, 100);
+	
+		//buttons
+		p5.fill(0);
+		p5.ellipse(buttonsx, buttonsy, 7);
+		p5.ellipse(buttonsx, buttonsy + 30, 7);
+		p5.ellipse(buttonsx, buttonsy + 60, 7);
 
-        p5.fill("orange")
-		p5.ellipse(x, y, 50);
-
-
-       // showMouseCoordinates();
+		//eyes
+		p5.triangle(320, 250, 320, 270, 340, 265)
+        p5.triangle(380, 250, 380, 270, 358, 265)
 
 	};
 
-    // const showMouseCoordinates = (p5, event) => {
-  
-    //     p5.fill(0)
-    //     p5.textSize(15)
-    //     p5.text("(" + p5.mouseX + ")" + "(" + p5.mouseY + ")", p5.mouseX, p5.mouseY)
-
-    //    }
 
 	return <Sketch setup={setup} draw={draw} />;
 };

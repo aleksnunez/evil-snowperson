1e9f2e002e2a5c0b9f6fc414031db48309f79db4
Alex Nunez <nunezaleks@gmail.com>
2021-11-19 19:17:46 -0800

    add routes for home page and play page

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,32 @@ import React, { useState } from 'react';
 import Figure from "./components/Sketch"
 import Header from './components/Header';
 import Word from './components/Word';
+import Select from './components/Select';
+
+import { BrowserRouter as Router, 
+         Route, 
+         Routes } from 'react-router-dom';
 
 const App = () => {
     const [correctLetters, setCorrectLetters] = useState([])
     //const [wrongLetters, setWrongLetters] = useState([])
     
     return (
-        <div>
+        <Router>
+            <div>
             <Header />
-            <Figure />
-            <Word correctLetters={correctLetters}/>
-            
-
-        </div>
+                <div>
+                    
+                    <Routes>
+                        <Route path="/" element={<><Select /></>}>
+                        </Route>
+                        <Route path="/play" element={<><Figure /></>}>
+                        </Route>
+                    </Routes>
+                </div>
+                <Word correctLetters={correctLetters}/>
+            </div>
+        </Router>
     )
 };
 

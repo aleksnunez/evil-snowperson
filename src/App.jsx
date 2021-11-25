b83b37b7bab4697b5b36799d915870b85a88a3f8
Alex Nunez <nunezaleks@gmail.com>
2021-11-24 21:11:43 -0800

    remove fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Figure from "./components/Sketch"
-import Header from './components/Header';
-import Word from './components/Word';
-import Select from './components/Select';
+import Figure from "./components/Sketch";
+import Header from "./components/Header";
+import Word from "./components/Word";
+import Select from "./components/Select";
 
-import { BrowserRouter as Router, 
-         Route, 
-         Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
-    const [correctLetters, setCorrectLetters] = useState([])
-    //const [wrongLetters, setWrongLetters] = useState([])
-    
-    return (
-        <Router>
-            <div>
-            <Header />
-                <div>
-                    
-                    <Routes>
-                        <Route path="/" element={<><Select /></>}>
-                        </Route>
-                        <Route path="/play" element={<><Figure /></>}>
-                        </Route>
-                    </Routes>
-                </div>
-                <Word correctLetters={correctLetters}/>
-            </div>
-        </Router>
-    )
+  const [correctLetters, setCorrectLetters] = useState([]);
+  //const [wrongLetters, setWrongLetters] = useState([])
+
+  return (
+    <Router>
+      <div>
+        <Header />
+        <div>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                  <Select />
+              }
+            ></Route>
+            <Route
+              path="/play"
+              element={
+                  <Figure />
+              }
+            ></Route>
+          </Routes>
+        </div>
+        <Word correctLetters={correctLetters} />
+      </div>
+    </Router>
+  );
 };
 
 export default App;

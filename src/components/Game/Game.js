110748c35ee5b81d4a3e72e3b7032939f7ab0b74
Alex <nunezaleks@gmail.com>
2023-09-17 17:50:09 -0400

    horizontally centered the p5 sketch + style changes to the header

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,37 +2,33 @@ import React from "react";
 import Sketch from "react-p5";
 import './Game.scss'
 
-	let buttonsx = 350;
-	let buttonsy = 240;
-
-	let bodyx = 350;
-
 const Game = () => {
 	const setup = (p5, canvasParentRef) => {
-		let canvas = p5.createCanvas(700, 600).parent(canvasParentRef);
+		let canvas = p5.createCanvas(p5.windowWidth, 600).parent(canvasParentRef);
 
-		canvas.parent('sketch-holder');
+		canvas.parent('game-container');
 	};
 
 	const draw = (p5) => {
 		p5.background(2, 7, 168);
 		p5.noStroke()
+		let width = p5.windowWidth;
         
 		//snowman
 		p5.fill(255)
-		p5.ellipse(bodyx, 400, 200, 200);
-		p5.ellipse(bodyx, 275, 150, 150);
-		p5.ellipse(bodyx, 165, 110, 100);
+		p5.ellipse(width / 2, 400, 200, 200);
+		p5.ellipse(width / 2, 275, 150, 150);
+		p5.ellipse(width / 2, 165, 110, 100);
 	
 		//buttons
 		p5.fill(0);
-		p5.ellipse(buttonsx, buttonsy, 7);
-		p5.ellipse(buttonsx, buttonsy + 30, 7);
-		p5.ellipse(buttonsx, buttonsy + 60, 7);
+		p5.ellipse(width / 2, 240, 7);
+		p5.ellipse(width / 2, 270, 7);
+		p5.ellipse(width / 2, 300, 7);
 
 		//eyes
-		p5.triangle(320, 150, 320, 170, 340, 165)
-    	p5.triangle(380, 150, 380, 170, 358, 165)
+		// p5.triangle(320, 150, 320, 170, 340, 165)
+    	// p5.triangle(380, 150, 380, 170, 358, 165)
 
 	};
 
